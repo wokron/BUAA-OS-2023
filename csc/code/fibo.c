98cdc09d975764ab93c92c995b6393fcf6b131cd
杨一堂 <21371326@buaa.edu.cn>
2023-02-27 16:21:16 +0800

    第四题2提交2

--- csc/code/fibo.c
+++ csc/code/fibo.c
@@ -1,12 +1,12 @@
 #include <fibo.h>
-char fibo(char n) {
-	char a;
-	char b;
-	char c;
+int fibo(int n) {
+	int a;
+	int b;
+	int c;
 	if (n == 1) {
 		a = 1;
 	} else {
-		char i;
+		int i;
 		c = 0;
 		b = 1;
 		for (i = 2; i <= n; i++) {
