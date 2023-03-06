9890f89e31ab2bf51b5dfc9b905bc7e974a89d71
OSome-Caesium <caesium@act.buaa.edu.cn>
2023-03-06 11:27:28 +0000

    init lab0-log-analysis

---
+++ analyze.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -eq 1 ];
+then
+    # Your code here. (1/4)
+
+else
+    case $2 in
+    "--latest")
+        # Your code here. (2/4)
+
+    ;;
+    "--find")
+        # Your code here. (3/4)
+
+    ;;
+    "--diff")
+        # Your code here. (4/4)
+
+    ;;
+    esac
+fi
