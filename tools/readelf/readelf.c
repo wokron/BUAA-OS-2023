06e7694e30aa0bda43eaa0e6824fe4863e0e9d07
杨一堂 <21371326@buaa.edu.cn>
2023-03-07 07:54:19 +0800

    readelf commit

--- tools/readelf/readelf.c
+++ tools/readelf/readelf.c
@@ -43,10 +43,10 @@ int readelf(const void *binary, size_t size) {
 	Elf32_Half sh_entry_count;
 	Elf32_Half sh_entry_size;
 	/* Exercise 1.1: Your code here. (1/2) */
-	sh_table = ehdr->e_entry + ehdr->e_shoff;
+	sh_table = binary + ehdr->e_shoff;
 	sh_entry_count = ehdr->e_shnum;
 	sh_entry_size = ehdr->e_shentsize;
-
+	// printf("%x %hu %hu\n", &sh_table, &sh_entry_count, &sh_entry_size);
 
 	// For each section header, output its index and the section address.
 	// The index should start from 0.
@@ -55,8 +55,9 @@ int readelf(const void *binary, size_t size) {
 		unsigned int addr;
 		/* Exercise 1.1: Your code here. (2/2) */
 		shdr = (Elf32_Shdr*)(sh_table + i * sh_entry_size);
+		// printf("shdr: %x\n", &shdr);
 		addr = shdr->sh_addr;
-
+		// addr = 0;
 		printf("%d:0x%x\n", i, addr);
 	}
 
