57579dd1dfb352c372963a4d1aa3aead72330ef7
杨一堂 <21371326@buaa.edu.cn>
2023-03-14 19:28:37 +0800

    lab2 pgdir_walk增加pgtable_entryp

--- kern/pmap.c
+++ kern/pmap.c
@@ -200,8 +200,9 @@ static int pgdir_walk(Pde *pgdir, u_long va, int create, Pte **ppte) {
 
 	/* Step 3: Assign the kernel virtual address of the page table entry to '*ppte'. */
 	/* Exercise 2.6: Your code here. (3/3) */
-	// *ppte = (Pte *)page2kva(pp);
-	*ppte = ((Pte *)KADDR(PTE_ADDR(*pgdir_entryp))) + PTX(va);
+	Pte *pgtable_entryp = (Pte *)KADDR(PTE_ADDR(*pgdir_entryp));
+	*ppte = pgtable_entryp + PTX(va);
+	
 	return 0;
 }
 
