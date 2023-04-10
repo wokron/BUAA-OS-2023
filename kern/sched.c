1b8685f0b368ea4d95ea593914e80fb24025bb96
杨一堂 <21371326@buaa.edu.cn>
2023-04-10 20:22:13 +0800

    lab3修改 schedule

--- kern/sched.c
+++ kern/sched.c
@@ -35,14 +35,13 @@ void schedule(int yield) {
 	 *   'TAILQ_FIRST', 'TAILQ_REMOVE', 'TAILQ_INSERT_TAIL'
 	 */
 	/* Exercise 3.12: Your code here. */
-	count--;
 	if (yield || count <= 0 || e == NULL || e->env_status != ENV_RUNNABLE) {
-		if (TAILQ_EMPTY(&env_sched_list)) {
-			panic("schedule: no runnable envs");
-		}
-
 		if (e != NULL && e->env_status == ENV_RUNNABLE) {
 			TAILQ_INSERT_TAIL(&env_sched_list, e, env_sched_link);
+		}
+
+		if (TAILQ_EMPTY(&env_sched_list)) {
+			panic("schedule: no runnable envs");
 		}
 
 		e = TAILQ_FIRST(&env_sched_list);
@@ -50,5 +49,6 @@ void schedule(int yield) {
 
 		count = e->env_pri;
 	}
+	count--;
 	env_run(e);
 }
