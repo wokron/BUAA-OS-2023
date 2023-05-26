197c34d754bccd19098b63f78ba00bbee82ba076
杨一堂 <21371326@buaa.edu.cn>
2023-05-26 17:17:09 +0800

    modify mkdir.c

--- user/mkdir.c
+++ user/mkdir.c
@@ -1,13 +1,29 @@
 #include <lib.h>
 
 void usage(void) {
-	printf("usage: mkdir [dir]\n");
+	printf("usage: mkdir [dir...]\n");
 	exit();
 }
 
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
+	int r;
+	int rt = 0;
+	struct Stat stat_buf;
+	if (argc < 2) {
 		usage();
 	}
-	return mkdir(argv[1]);
+
+	for (int i = 1; i < argc; i++) {
+		if (stat(argv[i], &stat_buf) >= 0) {
+			printf("%s already exists\n", argv[i]);
+			rt = -1;
+			continue;
+		}
+		if ((r = mkdir(argv[i])) < 0) {
+			printf("cannot create dir %s\n", argv[i]);
+			rt = r;
+		}
+	}
+
+	return rt;
 }
